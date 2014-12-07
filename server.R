786af8c95593180b4493aeac757802eed8e58a5e
jan-san <j.hagelauer@gmx.net>
2014-12-07 13:45:00 +0100

    Changed layout and added world map.

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(maps)
 
 cats <- data.table(flr=c(0, 15, 16, 18.5, 25, 30, 35, 40)
                    , cat=c('Very severely underweight'
@@ -29,19 +30,39 @@ e_sqr <- 0.00669437999013
 
 # Calculate the latitude-adjusted gravity
 g_lat <- function(lat) {
+    lat <- abs(lat)
     g_eq * (1 + k * sin(lat / 180 * pi) ^ 2) / sqrt(1 - e_sqr * sin(lat / 180 * pi) ^ 2)
 }
 
 
 shinyServer(
     function(input, output) {
+        
+        bmi_re <- reactive({
+            calcBmi(input$weight, input$height)
+        })
+        g_lat_re <- reactive({
+            g_lat(input$lat)
+        })
+        g_factor_re <- reactive({
+            g_lat(input$lat)/g_std
+        })
+        map_plot <- reactive({
+            par(mar=rep(0,times=4))
+            map('world', fill = TRUE, col = 1)
+            abline(h=input$lat, col='red', lwd=2)
+            box()
+        })
+        
         #bmi <- calcBmi(input$weight, input$height)
-        output$bmi  <- renderText ({calcBmi(input$weight, input$height)})
-        output$cat  <- renderText ({bmiCat(calcBmi(input$weight, input$height))} )
+        output$bmi  <- renderText ({bmi_re()})
+        output$cat  <- renderText ({bmiCat(bmi_re())})
 
-        output$weight_std <- renderText ({input$weight * g_lat(input$lat)/g_std} )
-        output$bmi_lat  <- renderText ({calcBmi(input$weight * g_lat(input$lat)/g_std, input$height)})
-        output$cat_lat  <- renderText ({bmiCat(calcBmi(input$weight * g_lat(input$lat)/g_std, input$height))} )
+        output$lat_weight_change <- renderText ({sprintf('%+.2f%%', (1-g_factor_re())*100)})
+        output$weight_std <- renderText ({input$weight * g_factor_re()})
+        output$bmi_lat  <- renderText ({bmi_re() * g_factor_re()})
+        output$cat_lat  <- renderText ({bmiCat(bmi_re() * g_factor_re())})
+        output$map_plot <- renderPlot({map_plot()}, height = 150, width = 250)
     }
 )
 
--- ui.R
+++ ui.R
@@ -4,26 +4,43 @@ shinyUI(pageWithSidebar(
         h3( 'Your input')
         , numericInput('weight', 'Your weight (in kg)',  80, min =  1, max = 200 , step =  0.000001)
         , numericInput('height', 'Your height (in cm)',  180, min =  1, max = 300 , step =  0.000001)
-        , numericInput('lat', 'Latitude of your current location (in °, between 0 (Equator) and 90 (Pole))',  0, min =  0, max = 90 , step =  0.0000001)
+        , numericInput('lat', 'Latitude of your current location (in °, between -90 (South Pole) and 90 (North Pole))',  0, min =  -90, max = 90 , step =  0.001)
+         , plotOutput("map_plot", width='250px')
     )
     , mainPanel(
-        h4('About the calculator')
-        , p('This application allows you to calculate your actual Body Mass Index anywhere in the world, adjusting for the weight difference introduced by the variation of Earth\'s gravity between the Equator and the Poles (see ', a(href='http://en.wikipedia.org/wiki/Gravity_of_Earth#Latitude', 'this article'), 'for details).')
-        , p('The adjusted BMI is based on your weight under standard gravity, i.e. the Earth\'s average gravity.')
-        , p('This can be useful for somewhat pedantic control freaks on a rigorous diet who frequently travel around the world and are forced to rely on scales not adjusted to the local gravity to determine their weight (huge market!).')
-        , p('Please note that the calculations assume that the weight was measured at sea level, and that the scale was calibrated for standard gravity.
-             Other factors influencing your local weight (such as buoyoncy influenced by ambient air pressure and your body\'s density as well as the position of sun and moon) are not considered in the calculation.')
-        , h4('Usage')
-        , p('Enter your weight, height and latitude into the input fields on the left. To obtain your current latitude, you can left click on your position in ',
-            a(href='http://maps.google.com', 'Google Maps'),
-            'or use a GPS tracker.')
-        
-        , h3( 'Unadjusted BMI')
-        , p(strong('BMI: '), textOutput( "bmi", inline=T ))
-        , p(strong('Category: '), textOutput( "cat", inline=T ))
-        , h3( 'LA-BMI')
-        , p(strong('Adjusted weight (under standard gravity): '), textOutput( "weight_std", inline=T ))
-        , p(strong('LA-BMI: '), textOutput( "bmi_lat", inline=T ))
-        , p(strong('Category: '), textOutput( "cat_lat", inline=T ))
+        tabsetPanel(
+            tabPanel('Results'
+                , h3( 'Unadjusted BMI')
+                , p(strong('BMI: '), textOutput( "bmi", inline=T ))
+                , p(strong('Category: '), textOutput( "cat", inline=T ))
+#                 , plotOutput("map_plot")
+                , h3( 'LA-BMI')
+                , p(strong('Deviation from standard graity at current location: '), textOutput( "lat_weight_change", inline=T ))
+                , p(strong('Adjusted weight (under standard gravity): '), textOutput( "weight_std", inline=T ), ' kg')
+                , p(strong('LA-BMI: '), textOutput( "bmi_lat", inline=T ))
+                , p(strong('Category: '), textOutput( "cat_lat", inline=T ))
+            )
+            , tabPanel('About'
+                , h4('About the calculator')
+                , p('This application allows you to calculate your actual Body Mass Index anywhere in the world, adjusting for the weight difference introduced by the variation of Earth\'s gravity between the Equator and the Poles (see ', a(href='http://en.wikipedia.org/wiki/Gravity_of_Earth#Latitude', 'this article'), 'for details).')
+                , p('The adjusted BMI is based on your weight under standard gravity, i.e. the Earth\'s average gravity.')
+                , p('This can be useful for somewhat pedantic control freaks on a rigorous diet who frequently travel around the world and are forced to rely on scales not adjusted to the local gravity to determine their weight (huge market!).')
+                , p('Please note that the calculations assume that the weight was measured at sea level, and that the scale was calibrated for standard gravity.
+                    Other factors influencing your local weight (such as buoyoncy influenced by ambient air pressure and your body\'s density as well as the position of sun and moon) are not considered in the calculation.')
+            )
+            , tabPanel('Usage'
+                       , h4('Usage')
+                       , p('Enter your weight, height and latitude into the input fields on the left. To obtain your current latitude, you can left click on your position in ',
+                           a(href='http://maps.google.com', 'Google Maps'),
+                           'or use a GPS tracker.')
+            )        
+            , tabPanel('FAQ'
+                       , h4('Frequently asked questions')
+                       , h5('1. Are you serious about this application?')
+                       , p('Of course!')
+                       , h5('2. Seriously?')
+                       , p('Okay...maybe not 100% serious. But how serious can you be about an obesity measure that classifies professional athletes with ridiculously low body fat as obese?')
+            )        
+        )
     )
 ))
